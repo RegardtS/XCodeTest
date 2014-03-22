99062f6640bdb39d33edadbefa15a15c357582bd
Regardt Schindler <regardt@myself.com>
2014-03-22 17:06:36 +0200

    Initial Commit

---
+++ TestGitProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestGitProject
+//
+//  Created by Saint on 2014/03/22.
+//  Copyright (c) 2014 Saint. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
